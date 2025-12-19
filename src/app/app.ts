059c5a3895f635a2490f28a572b5133b69d20cc4
Harun Jaganjac <harun2003jaganjac@gmail.com>
2025-12-19 19:37:31 +0100

    Added Firebase services with test

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,8 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { Firestore, collection, addDoc } from '@angular/fire/firestore';
+
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
@@ -9,4 +11,17 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('WP_2_Harun_Jaganjac');
+
+  constructor(private firestore: Firestore) {}
+
+  async testInsert() {
+    const usersRef = collection(this.firestore, 'test-users');
+
+    await addDoc(usersRef, {
+      name: 'Harun Test',
+      createdAt: new Date()
+    });
+
+    alert('Upisano u Firestore ✔️');
+  }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyB77JfHaIvuM99RtIW3tL0L-85xdMCBV2UQ",
+    authDomain: "wp2dashboard.firebaseapp.com",
+    projectId: "wp2dashboard",
+    storageBucket: "wp2dashboard.appspot.com",
+    messagingSenderId: "985087902350",
+    appId: "1:985087902350:web:cad921421ed34b2353bfdf",
+    measurementId: "G-3VTYGRHZ89"
+  }
+}
